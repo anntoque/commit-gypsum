94a66e213f84dfdb087e8c6744fa259ba1cb894c
yamamotoya <yamamotoya@istyle.co.jp>
2018-10-19 02:21:02 +0900

    パンチカードのcommit数取得

--- commit-gypsum.sh
+++ commit-gypsum.sh
@@ -6,7 +6,8 @@ REPO_LIST=`curl -u :${ACCESS_TOKEN} https://api.github.com/user/repos | jq '.[].
 
 for owner_repo in ${REPO_LIST[@]}
 do
+  echo "${owner_repo}"
   OWNER_REPO_URL=`echo ${owner_repo} | sed 's/"//g'`
  # curl -u :${ACCESS_TOKEN} https://api.github.com/repos/${OWNER_REPO_URL}/stats/punch_card | jq '.['${TODAY}'] | .[2]'
- curl -u :${ACCESS_TOKEN} https://api.github.com/repos/${OWNER_REPO_URL}/stats/punch_card | jq '.[]' | jq '.[2]'
+ curl -u :${ACCESS_TOKEN} https://api.github.com/repos/${OWNER_REPO_URL}/stats/punch_card | jq '.[] | select(.[0]=='${TODAY}') | .[2]'
 done
