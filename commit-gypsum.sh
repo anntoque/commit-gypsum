3adf61bc740dfc3f5bf5ff5376b5bad9c1a4ca6d
yamamotoya <yamamotoya@istyle.co.jp>
2018-10-21 18:22:10 +0900

    メッセージをslackに通知するように設定

--- commit-gypsum.sh
+++ commit-gypsum.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 TODAY=`date "+%w"`
 ACCESS_TOKEN=${1}
+CURRENT_DATE=`date "+%Y-%m-%d"`
 
+COMMITED_MESSAGE="ナイス！コミット！"
+NOT_COMMITED_MESSAGE="なぜcommitしない"
+WEBHOOK_URL=${2}
 LATEST_COMMIT_DATE=`curl -u :${ACCESS_TOKEN} https://api.github.com/users/anntoque/events | \
 jq '.[0] | select(.["type"]=="PushEvent")' | jq '.["created_at"]' | cut -c 2-11`
 
-CURRENT_DATE=`date "+%Y-%m-%d"`
-
 if [ "${LATEST_COMMIT_DATE}" = "${CURRENT_DATE}" ]; then
-  echo "今日もcommitしたな。お疲れ様"
+  echo ${COMMITED_MESSAGE}
 else
-  echo "なぜcomiitしない！！"
+  echo ${NOT_COMMITED_MESSAGE}
+  curl -X POST --data-urlencode "payload={\"channel\": \"#test-webhook\", \"username\":\"webhookbot\",\
+   \"text\": \"${NOT_COMMITED_MESSAGE}\", \"icon_emoji\": \":warning:\"}" ${WEBHOOK_URL}
 fi
