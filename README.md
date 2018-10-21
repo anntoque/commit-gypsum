a8326072868706576d1deac2519b58a6f6bd19b9
yamamotoya <yamamotoya@istyle.co.jp>
2018-10-21 18:48:00 +0900

    READMEに概要・仕様・インストール方法を追記

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # commit-gypsum
+**commit-gypsum** はcommitを毎日行なっているかをチェックすることで、使用者の成長を監視する仕組みです。
+
+## 仕様
+- 実行時の日と最新のgithubへpushされたcommitの日を比較し、その日まだcommitしてなければslackに通知します
+- 実行時の日にcommit済みであれば、賞賛のメッセージが送られます。
+- 逆にだcommitしていなければ、commitを促すメッセージが送られます
+
+## インストール・使用方法
+```
+$ git clone git@github.com:qw3rtman/git-fire.git
+````
--- commit-gypsum.sh
+++ commit-gypsum.sh
@@ -3,16 +3,16 @@ TODAY=`date "+%w"`
 ACCESS_TOKEN=${1}
 CURRENT_DATE=`date "+%Y-%m-%d"`
 
-COMMITED_MESSAGE="ナイス！コミット！"
-NOT_COMMITED_MESSAGE="なぜcommitしない"
+COMMITED_MESSAGE="毎日1commit…その積み重ねが大事！お疲れ様自分！"
+NOT_COMMITED_MESSAGE="なぜcommitしないいいいいいいい今すぐしろおおおおおおお"
 WEBHOOK_URL=${2}
 LATEST_COMMIT_DATE=`curl -u :${ACCESS_TOKEN} https://api.github.com/users/anntoque/events | \
 jq '.[0] | select(.["type"]=="PushEvent")' | jq '.["created_at"]' | cut -c 2-11`
 
 if [ "${LATEST_COMMIT_DATE}" = "${CURRENT_DATE}" ]; then
-  echo ${COMMITED_MESSAGE}
+  curl -X POST --data-urlencode "payload={\"channel\": \"#test-webhook\", \"username\":\"webhookbot\",\
+   \"text\": \"${COMMITED_MESSAGE}\", \"icon_emoji\": \":man-lifting-weights:\"}" ${WEBHOOK_URL}
 else
-  echo ${NOT_COMMITED_MESSAGE}
   curl -X POST --data-urlencode "payload={\"channel\": \"#test-webhook\", \"username\":\"webhookbot\",\
    \"text\": \"${NOT_COMMITED_MESSAGE}\", \"icon_emoji\": \":warning:\"}" ${WEBHOOK_URL}
 fi
