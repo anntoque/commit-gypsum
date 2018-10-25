28d9ed51fbb9aafd8d81c8c6cdfd38b4860c23fd
Anntoque <ann.toque@gmail.com>
2018-10-25 14:59:54 +0000

    config.shがなければ作成&必要な情報を対話形式で入力

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 yum install epel-release
 yum install jq
+
+if[! -e config.sh]; then
+  touch config.sh
+fi
+
+echo "please input github access token"
+read ACCESS_TOKEN
+
+echo "please input github slack webhook"
+read SLACK_WEBHOOK
+
+echo "GITHUB_TOKEN=${ACCESS_TOKEN}" >> config.sh
+echo "SLACK_WEBHOOK=${SLACK_WEBHOOK}" >> config.sh
