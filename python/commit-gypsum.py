ee4109779f4187359a00a863cd1200fedcb95893
anntoque <ann.toque@gmail.com>
2019-01-12 16:13:00 +0900

    pythonで日時取得してslackに送信

--- python/commit-gypsum.py
+++ python/commit-gypsum.py
@@ -1,5 +1,9 @@
 import slackweb
 import config
+from datetime import datetime
 
 slack = slackweb.Slack(url=config.SLACK_WEBHOOK)
-slack.notify(text="pythonからslackpytttt")
+
+current_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+slack_message = "pythonからslackpytttt" + current_date
+slack.notify(text=slack_message)
