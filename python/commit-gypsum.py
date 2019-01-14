7f0ce4a4ca172f766e59ed4eca9d97d3cde22536
anntoque <ann.toque@gmail.com>
2019-01-15 00:20:10 +0900

    関数化とgithub apiを叩く処理実装

--- python/commit-gypsum.py
+++ python/commit-gypsum.py
@@ -1,10 +1,24 @@
 import slackweb
 import config
+import requests
 from datetime import datetime
 
+current_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 slack = slackweb.Slack(url=config.SLACK_WEBHOOK)
 github_token = config.GITHUB_TOKEN
+headers = {'Authorization': 'token ' + github_token}
 
-current_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-slack_message = "pythonからslackpytttt" + current_date
-slack.notify(text=slack_message)
+def get_last_commit(): 
+    response = requests.get(
+        'https://api.github.com/users/anntoque/events',
+        headers=headers
+    )
+    print(response.text)
+
+def send_message(): 
+    slack_message = "pythonからslackpytttt" + current_date
+    slack.notify(text=slack_message)
+
+if __name__ == '__main__':
+    get_last_commit()
+    send_message()
