ee540a83acf4357729990e8707c28fed48ae4ba6
anntoque <ann.toque@gmail.com>
2019-01-15 01:23:46 +0900

    最後のイベントの日時を取得、日本時間に変換

--- python/commit-gypsum.py
+++ python/commit-gypsum.py
@@ -2,23 +2,35 @@ import slackweb
 import config
 import requests
 from datetime import datetime
+import datetime as dt
 
-current_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+now = dt.datetime.now()
+current_date = now.strftime("%Y/%m/%d %H:%M:%S")
 slack = slackweb.Slack(url=config.SLACK_WEBHOOK)
 github_token = config.GITHUB_TOKEN
 headers = {'Authorization': 'token ' + github_token}
 
-def get_last_commit(): 
+def get_github_events(): 
     response = requests.get(
         'https://api.github.com/users/anntoque/events',
-        headers=headers
-    )
-    print(response.text)
+        headers=headers)
 
-def send_message(): 
+    return response.json()
+
+def get_last_commit(events):
+    event_date = events[0]["created_at"]
+    event_date_dt = dt.datetime.strptime(event_date, '%Y-%m-%dT%H:%M:%SZ')
+
+    event_jp_date= event_date_dt + dt.timedelta(hours=9)
+
+    return event_jp_date
+
+def send_message():
     slack_message = "pythonからslackpytttt" + current_date
     slack.notify(text=slack_message)
 
 if __name__ == '__main__':
-    get_last_commit()
+    events = get_github_events()
+    # last_commit = get_last_commit(events)
+    last_commit_date = get_last_commit(events)
     send_message()
