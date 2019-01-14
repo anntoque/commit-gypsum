bde123da31a481e244264468f9a7531886c27f1a
anntoque <ann.toque@gmail.com>
2019-01-14 23:50:50 +0900

    githubのアクセストークンを読み込む

--- python/commit-gypsum.py
+++ python/commit-gypsum.py
@@ -3,6 +3,7 @@ import config
 from datetime import datetime
 
 slack = slackweb.Slack(url=config.SLACK_WEBHOOK)
+github_token = config.GITHUB_TOKEN
 
 current_date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 slack_message = "pythonからslackpytttt" + current_date
